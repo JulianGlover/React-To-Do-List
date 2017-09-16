f47217a549fe282fa37c1e1d54c5098db94f4282
Julian <julian.glover94@yahoo.com>
2017-09-16 13:10:50 -0400

    Added test case for the website index page

--- ToDoList/tests.py
+++ ToDoList/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+class ToDoListViewsTestCase(TestCase):
+
+    #Test to ensure that the application returns the index page and correct index template.
+    def test_index(self):
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'ToDoList/index.html')

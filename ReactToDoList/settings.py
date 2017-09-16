984e481a9053d7b57fc9d0290917308e6f35a5df
Julian <julian.glover94@yahoo.com>
2017-09-16 14:43:59 -0400

    Updated Procfile

--- ReactToDoList/settings.py
+++ ReactToDoList/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'p3#)3-&df$75h&dlu60vr+giz5v_8cedoc9h^d9i=a7y+^cgn)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
